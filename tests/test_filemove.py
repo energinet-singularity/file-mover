0d47305d5ff30b824c0b081866e2be5af03838e7
Karsten Rasmussen <bolsmand@gmail.com>
2021-12-16 17:25:40 +0100

    Improve formatting of test-file

--- tests/test_filemove.py
+++ tests/test_filemove.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-import sys, os, pytest
+import sys
+import os
+import pytest
 
 # Adding parent dir to path to find and load module/file 'forecast_parser' in parent dir
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from filemover import *
 
 
-# Dummy-test which will always succeed - must be replaced by real tests
+# Dummytest which will always succeed - must be replaced by real tests
 def test_dummy():
-    assert = True
+    assert True
